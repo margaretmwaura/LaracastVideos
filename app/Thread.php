8cdaa2f20511f8dd4491cdd03bc6c176e7a5fb70
Margaret Mwaura <mwauramargaret1@gmail.com>
2020-01-31 18:02:47 +0300

    A thread can have replies

--- app/Thread.php
+++ app/Thread.php
@@ -11,4 +11,8 @@ class Thread extends Model
     {
         return '/threads/'.$this->id;
     }
+//    public function replies()
+//    {
+//        return $this->hasMany(Reply::class);
+//    }
 }
