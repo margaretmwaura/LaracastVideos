73ea80b8e2a522fedc3d4136bd974f32ecc4b51f
Margaret Mwaura <mwauramargaret1@gmail.com>
2020-02-01 14:23:19 +0300

    Lets make some Testing helpers

--- tests/Feature/CreateThreads.php
+++ tests/Feature/CreateThreads.php
@@ -16,18 +16,22 @@ class CreateThreads extends TestCase
      */
     /** @test */
     use DatabaseMigrations;
+
+    /** @test */
     function guests_may_not_create_thread()
     {
         $this->expectException('Illuminate\Auth\AuthenticationException');
-        $thread=factory('App\Thread')->make();
+        $thread=make('App\Thread');
         $this->post('/threads',$thread->toArray());
     }
+
+    /** @test */
     public function an_authenticated_user_can_create_new_forum_threads()
     {
 
-        $this->actingAs(factory('App\User')->create());
+        $this->signIn();
 
-        $thread=factory('App\Thread')->make();
+        $thread=make('App\Thread');
         $this->post('/threads',$thread->toArray());
         $this->get($thread->path())
             ->assertSee($thread->title)
