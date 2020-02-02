b6761f3ad913453487e18c9314bf7610c21ac513
Margaret Mwaura <mwauramargaret1@gmail.com>
2020-02-02 18:22:53 +0300

    applying global scopes to reduce queries called

---
+++ app/Favoritable.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace App;
+
+
+trait Favoritable
+{
+
+    public function isFavorited()
+    {
+        return !!$this->favorites->where('user_id', auth()->id())->count();
+    }
+
+    public function getFavoritesCountAttribute()
+    {
+        return $this->favorites->count();
+    }
+
+    public function favorites()
+    {
+        return $this->morphMany(Favorite::class, 'favorited');
+    }
+
+    public function favorite()
+    {
+        if (!$this->favorites()->where(['user_id' => auth()->id()])->exists()) {
+            return $this->favorites()->create(['user_id' => auth()->id()]);
+        }
+
+    }
+}
