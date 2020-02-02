296e09ef83b384bb6a1f2519cc2aa20620a5795f
Margaret Mwaura <mwauramargaret1@gmail.com>
2020-02-03 02:37:04 +0300

    a users activity feed should include favorited replies

--- app/Favoritable.php
+++ app/Favoritable.php
@@ -7,6 +7,7 @@ namespace App;
 trait Favoritable
 {
 
+
     public function isFavorited()
     {
         return !!$this->favorites->where('user_id', auth()->id())->count();
--- app/Favorite.php
+++ app/Favorite.php
@@ -7,5 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Favorite extends Model
 {
     //
+    use RecordsActivity;
     protected $guarded = [];
+
+    public function favorited()
+    {
+        return $this->morphTo();
+    }
 }
