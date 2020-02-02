b8102c506fc4247764dfb70131098d07aa1fe668
Margaret Mwaura <mwauramargaret1@gmail.com>
2020-02-02 07:37:08 +0300

    a user can filter by popular threads

--- app/Filters/ThreadFilters.php
+++ app/Filters/ThreadFilters.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class ThreadFilters extends Filters
 {
 
-    protected $filters = ['by'];
+    protected $filters = ['by','popular'];
     /**
      * @param $builder
      * @param $username
@@ -19,4 +19,9 @@ class ThreadFilters extends Filters
         return $this->builder->where('user_id', $user->id);
     }
 
+    protected function popular($username)
+    {
+        $this->builder->getQuery()->orders=[];
+        return $this->builder->orderBy('replies_count', 'desc');
+    }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,8 +12,19 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <style>
+        body{
+            padding-bottom: 100px
+        }
+        .level{
+            display: flex; align-items: center;
+        }
+        .flex {
+            flex: 1;
+        }
+    </style>
 </head>
-<body style="padding-bottom: 100px">
+<body >
     <div id="app">
         <nav class="navbar navbar-default navbar-static-top">
             <div class="container">
@@ -46,6 +57,7 @@
                                 @if(auth()->check())
                                 <li><a href="/threads?by={{ auth()->user()->name }}">My threads</a></li>
                                 @endif
+                                <li><a href="/threads?popular=1">Popular threads</a></li>
                             </ul>
                         </li>
                         <li class="dropdown">
