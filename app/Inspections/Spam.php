282580732f4de22286033c20e5ef98d7af76e1e3
Margaret Mwaura <mwauramargaret1@gmail.com>
2020-02-16 16:52:43 +0300

    refactoring spam detection methods into their own classes

---
+++ app/Inspections/Spam.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace App\Inspections;
+
+
+class Spam
+{
+
+    protected $inspections=[
+        InvalidKeyWords::class,
+        KeyHeldDown::class
+    ];
+    public function detect($body)
+    {
+        foreach ($this->inspections as $inspection) {
+            app($inspection)->detect($body);
+        }
+
+        return false;
+
+    }
+
+}
--- tests/Unit/SpamTest.php
+++ tests/Unit/SpamTest.php
@@ -2,7 +2,7 @@
 
 namespace Tests\Feature;
 
-use App\Spam;
+use App\Inspections\Spam;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
@@ -11,11 +11,21 @@ class SpamTest extends TestCase
 {
 
      /** @test */
-    public function it_validates_spam()
+    public function it_checks_for_invalid_keywords()
     {
         $spam = new Spam();
 
         $this->assertFalse($spam->detect('Inncocent reply here'));
+
+    }
+
+    /** @test */
+    public function it_checks_for_any_key_being_held_down()
+    {
+        $spam = new Spam();
+
+        $this->expectException('Exception');
+        $spam->detect('Hello world qqqqqqqqqqqqq');
 
     }
 }
